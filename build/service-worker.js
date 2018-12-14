1f4b371a97e6ed58690ef300c46037d8599f9740
Ivan De Menezes <ivandmenezes@gmail.com>
2018-12-14 11:13:43 -0400

    page

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kavana/precache-manifest.c869e9c08a12f73d7ce3e70d8fe0f5c2.js"
+  "./precache-manifest.06e94950d38fd4e8c41bc40e78e0175b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kavana/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
